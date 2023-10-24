db4f2a072c763287ab67e26dc7b0e1dc7c529fc8
yukotanjung <yukotanjung@gmail.com>
2023-10-24 13:32:17 +0700

    fixing design coding part 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,70 +15,5 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(),
       home: const DetailScreen(),
     );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-            child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Container(
-          margin: const EdgeInsets.only(top: 16.0),
-          child: const Text(
-            'Wisata Petik Hikmah',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 16.0),
-          child: const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Icon(Icons.calendar_today),
-                    SizedBox(
-                      height: 8.0,
-                    ),
-                    Text('Buka kecuali libur'),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Icon(Icons.access_time),
-                    SizedBox(
-                      height: 8.0,
-                    ),
-                    Text('09:00 - 20:00'),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Icon(Icons.monetization_on),
-                    SizedBox(
-                      height: 8.0,
-                    ),
-                    Text('Rp. 2.000,00'),
-                  ],
-                ),
-              ]),
-        ),
-        Container(
-          padding: const EdgeInsets.all(16.0),
-          child: const Text(
-            'Berada di jalur yang mudah diakses Wisata Petik Hikmah sangat digemari oleh orang yang sabar ketika diberikan cobaan.',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 16.0),
-          ),
-        )
-      ],
-    )));
   }
 }
